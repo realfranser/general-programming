dcf7cb51b72520e367e107ce1f7d50784419da0c
realfranser <serranoarresejavier@gmail.com>
2021-12-16 00:25:37 +0100

    first approach

---
+++ web-dev/learning-react/src/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import { SquareProps } from './interfaces';
+
+class Square extends React.Component<SquareProps> {
+  render() {
+    const { value } = this.props;
+    return <button className="square">{value}</button>;
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(value: number) {
+    return <Square value={value} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
---
+++ web-dev/learning-react/src/interfaces.ts
@@ -0,0 +1,3 @@
+export interface SquareProps {
+  value: number;
+}
